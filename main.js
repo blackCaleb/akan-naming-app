815027770c0058654342d362e0e4f1a9fb10fd93
blackCaleb <bblackcaleb@gmail.com>
2021-11-17 00:33:23 +0300

    fixed a known bug

--- main.js
+++ main.js
@@ -28,12 +28,6 @@ console.log(curday('-'));
         year.appendChild(option);
     }
   };
-  
-  // $(document).ready(function(){
-  //   var year = $('#year');
-  //   var month = $('#month');
-  //   var date = $('#date');
-  //   var gender = $('#gender');
       var yearbirth = document.getElementById("year");
       var monthbirth = document.getElementById("month");
       var daybirth = document.getElementById("dob");
@@ -46,12 +40,12 @@ console.log(curday('-'));
     // attaching a function to the event listener
     var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var day = week[new Date(year.val()+'/'+month.val()+'/'+dob.val()).getDay()];
-    var name;
+    // var name;
 
     // the condition statements
 
     // for Female gender
-    if (gender.val() === "Female"){
+    if (gender.checked() == "Female"){
 
       if(day === "Sunday"){
         name = "Akosua"
@@ -77,7 +71,7 @@ console.log(curday('-'));
     }
 
     //for Male gender
-    if (gender.val() === "Male"){
+    if (gender.checked() === "Male"){
 
       if(day === "Sunday"){
         name = "Kwasi"
@@ -100,22 +94,11 @@ console.log(curday('-'));
       else if(day === "Saturday"){
         name = "Kwame"
       }
-    };
-
-    // validating data
-    if(year.val() === ''){
-      $('#year-err').html("Please select your year of birth");
-    }
-    else if (month.val() === ''){
-      $('#month-err').html("Please select your month of birth");
-    }
-    else if (day.val() === ''){
-      $('#dob-err').html("Please select your day of birth");
     }
 
     // end of condition
 
     else{
-      return('#response').html('you were born on' + day + 'and your akan name is' + name);
+      Display.innerHTML = "Your akan name is" + name + "and you were born on" + day
     }
     }
